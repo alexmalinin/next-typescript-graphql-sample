bbcd1233566812f2a68c38dcbf8c012ef0ffd6ba
alexmalinin <amalinin.codica@gmail.com>
2019-02-10 02:15:56 +0200

    [Feature] Apollo server, ANTd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,46 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import * as React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
 
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+
+      <Mutation
+        mutation={gql`
+          mutation {
+            login(email: "test@test.test", password: "password") {
+              id
+              name
+              firstName
+              lastName
+              email
+            }
+          }
+        `}
+      >
+        {mutate => (
+          <button 
+            onClick={async () => {
+              const response = await mutate();
+
+              console.log(response);
+            }}
+          >
+            Login
+          </button>
+        )}
+      </Mutation>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
